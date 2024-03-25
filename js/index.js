f534bc34df408c07fdd6d031335ef93d3d625cd6
jayathracm <jayathracm@gmail.com>
2024-03-25 13:54:23 +0200

    added a new html page for creating posts, added response when clicking create account at the login popup and vice versa.

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    var loginButton = document.getElementById('loginButton');
+    loginButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default action of the button
+
+        var email = document.getElementById('emailInput').value;
+        var password = document.getElementById('passwordInput').value;
+        var warningMessage = document.getElementById('warningMessage');
+
+    if (email.trim() === '' || password.trim() === '') {
+        warningMessage.innerText = 'Invalid email or password!';
+        return false;
+    } else {
+        warningMessage.innerText = '';
+        return true;
+    }
+})
+})
+
+document.getElementById('loginButton').addEventListener('click', function(event) {
+    event.preventDefault();
+    validateLoginForm();
+})
