063f1b028a6f9891685071a71a785f98f922f0c6
toshi-Devs <trahim87mustapha@gmail.com>
2024-04-03 16:12:04 +0300

    sql format

--- server/db.sql
+++ server/db.sql
@@ -1,25 +1,21 @@
--- Creating a new database named 'Memehub_project'
-CREATE DATABASE Memehub_project;
 
--- Using the newly created database
-USE Memehub_project;
+drop table posts;
+drop table users;
 
--- Creating a table named 'UserInformation'
-CREATE TABLE UserInformation (
-    PersonID SERIAL PRIMARY KEY,
-    Email VARCHAR(255) NOT NULL,
-    Password VARCHAR(255) NOT NULL,
-    Username VARCHAR(255) NOT NULL,
-    REGISTERED_ON TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+create table users (
+	id BIGSERIAL PRIMARY KEY NOT NULL,
+	username VARCHAR(200) NOT NULL,
+	email VARCHAR(200) NOT NULL,
+	password VARCHAR(200) NOT NULL,
+	UNIQUE (email),
+	UNIQUE (username)
 );
+select * from users
 
--- Selecting all records from the 'UserInformation' table
--- (Initially empty, as we've just created it)
-SELECT * FROM UserInformation;
-
--- Inserting a new user into the 'UserInformation' table
-INSERT INTO UserInformation (Email, Password, Username)
-VALUES ('mustafa@gmail.com', 'Mustafa1234', 'Mustafa');
-
--- Verify if the user has been added correctly
-SELECT * FROM UserInformation;
+CREATE TABLE posts (
+  id BIGSERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  interest VARCHAR(200) NOT NULL,
+  url VARCHAR(255) NOT NULL
+);
+select * from posts
