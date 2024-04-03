f2a61aa1247ef1808117261f1e8b336b90c2d193
aleksi loddo <143383551+Aleksi-Loddo@users.noreply.github.com>
2024-04-03 16:24:43 +0300

    added description for sql

--- server/db.sql
+++ server/db.sql
@@ -1,6 +1,10 @@
 
+-- drop tables
+
 drop table posts;
 drop table users;
+
+-- create users table
 
 create table users (
 	id BIGSERIAL PRIMARY KEY NOT NULL,
@@ -12,10 +16,16 @@ create table users (
 );
 select * from users
 
+-- create posts table
+
 CREATE TABLE posts (
   id BIGSERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
   interest VARCHAR(200) NOT NULL,
   url VARCHAR(255) NOT NULL
 );
+
+-- select all posts
+
 select * from posts
+
